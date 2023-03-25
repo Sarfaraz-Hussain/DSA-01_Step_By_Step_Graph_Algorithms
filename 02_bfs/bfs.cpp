840962c660500ff245754045534d05668756e096
Sarfaraz-Hussain <call2me786@gmail.com>
2023-03-25 16:42:34 +0530

    coded BFS algorithm

---
+++ 02_bfs/bfs.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define pb push_back
+#define F first
+#define S second
+
+using  namespace std;
+
+class Graph {
+
+	int V;
+	list<int> *l;
+public:
+	Graph(int V) {
+		this -> V = V;
+		l = new list<int>[V];
+	}
+
+	void addEdge(int u, int v, bool undir = true) {
+		l[u].push_back(v);
+		if (undir) {
+			l[v].push_back(u);
+		}
+	}
+
+	void bfs(int src) {
+		queue<int> q;
+		q.push(src);
+		bool visited[V] {false};
+		visited[src] = true;
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			cout << node << " ";
+			for (auto nbr : l[node]) {
+				if (!visited[nbr]) {
+					q.push(nbr);
+					visited[nbr] = true;
+				}
+			}
+		}
+	}
+
+};
+
+int32_t main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int V, E, src;
+	cin >> V >> E >> src;
+	Graph graph(V);
+	while (E--) {
+		int u, v;
+		cin >> u >> v;
+		graph.addEdge(u, v);
+	}
+
+	graph.bfs(src);
+	return 0;
+
+}
