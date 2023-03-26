0751fbe8676ccba152cc341549378da2e4ce027e
Sarfaraz-Hussain <call2me786@gmail.com>
2023-03-26 17:47:37 +0530

    added code for single source shortest path

---
+++ 03_Single_Source_Shortest_Path/ShortestPath.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define pb push_back
+#define F first
+#define S second
+
+using  namespace std;
+
+class Graph {
+	int V;
+	list<int> *l;
+public:
+	Graph(int V) {
+		this -> V = V;
+		l = new list<int>[V];
+	}
+	void addEdge(int u, int v, bool undir = true) {
+		l[u].push_back(v);
+		if (undir) {
+			l[v].push_back(u);
+		}
+	}
+
+	void singleSourceShortestPath(int src, int dest = -1) {
+		queue<int> q;
+		q.push(src);
+		bool visited[V] {false};
+		visited[src] = true;
+		int parent[V] { -1};
+		parent[src] = src;
+		int distance[V] {0};
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			for (auto nbr : l[node]) {
+				if (!visited[nbr]) {
+					parent[nbr] = node;
+					distance[nbr] = distance[node] + 1;
+					q.push(nbr);
+					visited[nbr] = true;
+				}
+			}
+		}
+		for (int i = 0; i < V; i++) {
+			cout << "Distance from src " << src << " to " << i << " is " << distance[i] << endl;
+		}
+		if (dest != -1) {
+			int temp = dest;
+			cout << temp << " --> ";
+			while (temp != src) {
+				temp = parent[temp];
+				cout << temp << " --> ";
+			}
+			cout << src << endl;
+		}
+	}
+};
+
+
+int32_t main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int V, E, src;
+	cin >> V >> E >> src;
+	Graph graph(V);
+	while (E--) {
+		int u, v;
+		cin >> u >> v;
+		graph.addEdge(u, v);
+	}
+
+	graph.singleSourceShortestPath(src, 6);
+
+}
